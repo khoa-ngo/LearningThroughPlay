85e61c2de71c0e4bbca93f0d471ec12987a1146d
Khoa Ngo <khoa.ngo@me.com>
2018-07-09 10:18:47 -0700

    added more data visualization

--- log_analysis.py
+++ log_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # filename = 'ai_physical_log.csv'
 filename = 'ai_simulated_log.csv'
@@ -13,9 +14,14 @@ for index, row in df.iterrows():
     acceleration.append(velocity-last_velocity)
     last_velocity = velocity
 
-print(acceleration)
+# print(acceleration)
 # print(df['Velocity'])
 # df.plot(kind='scatter', y='Velocity')
 # for _ in
-df.plot(y='Velocity')
-# plt.show()
+# df.plot(y='Velocity')
+
+data = acceleration
+colors = np.random.rand(len(data))
+plt.figure(dpi=500)
+plt.scatter(range(len(data)), data, c=colors, alpha=0.2)
+plt.show()
