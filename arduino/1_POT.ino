a483c4b6c2dc5f6bf3fa874c7b10a7dbce5dd9fc
Khoa Ngo <khoa.ngo@me.com>
2018-09-18 19:35:55 -0400

    updated arduino code for new hardware

--- arduino/1_POT.ino
+++
@@ -1,3 +0,0 @@
-float getPosition(int POT) {
-  return 1 - analogRead(POT)*(1.0/1023.0);
-}
--- arduino/3_SERVO.ino
+++
@@ -1,9 +0,0 @@
-void servoResetPosition() {
-  servo2.write(129); //Right, default: 130
-  servo1.write(34); //Left, default: 33
-}
-
-void servoActivePosition() {
-  servo2.write(10); //Right, default: 10
-  servo1.write(153); //Left, default: 153
-}
